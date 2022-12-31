85e99048ebd3f4dcfe5ad7a2b128663966c068df
Daniel Vitor <danielvitorffaria@gmail.com>
2022-12-30 23:55:58 -0300

    main

--- Codding/routes/produtor.js
+++ Codding/routes/produtor.js
@@ -1,35 +1,45 @@
 const express = require('express')//Estabelece dependência com o express e seu modulador de rotas
 const router = express.Router()
 const mysql = require('../config/db').pool//Estabelece depencia com a pool de conexões do banco de dados
-
-
+const auth = require('basic-auth')//Estabelece dependencia com o basic auth
 
 /*CADASTRAR NOVO PRODUTOR*/
 router.post('/cadastrar',(req,res,next)=>{ //Inserir Novo Usuário        
+        const user = auth(req)
             mysql.getConnection((err,con)=>{//Conectar com MySql
                 if(err){//Retorna erro em caso de existir
                     res.send({
                         error: err
                     })
                 }
-                con.query( 
-                    'INSERT INTO produtores (nomeProdutor, cpfProdutor, idUsuario) VALUES (?,?,?)',//Insere na Base de Dados os valores que serão enviados pelo POST
-                    [req.body.nomeProdutor, req.body.cpfProdutor, req.body.idUsuario],
-                    (err,result,fields)=>{
-                        con.release()
-                        if(err){//Tratativa de Erro
-                            return res.status(500).send({
-                                error: err,
-                                response: result
-                                })
+                con.query('SELECT idUsuario FROM usuarios WHERE nomeUsuario = (?)',
+                [user.name],(err,result,fields)=>{
+                    con.release()
+                    for(let i = 0;i<result.length;i++){
+                        if(result[0].idUsuario!==undefined){
+                            const idUsuario = result[0].idUsuario
+                            con.query( 
+                                'INSERT INTO produtores (nomeProdutor, cpfProdutor, idUsuario) VALUES (?,?,?)',//Insere na Base de Dados os valores que serão enviados pelo POST
+                                [req.body.nomeProdutor, req.body.cpfProdutor, idUsuario],
+                                (err,result,fields)=>{
+                                    con.release()
+                                    if(err){//Tratativa de Erro
+                                        return res.status(500).send({
+                                            error: err,
+                                            response: result
+                                            })
+                                    }
+                                    res.status(201).send({//Retorna mensagem positiva e o id Inserido
+                                        mensagem: "Produtor Cadastrado",
+                                        idProdutor: result
+                                    })
+                                }
+                            )
                         }
-                        res.status(201).send({//Retorna mensagem positiva e o id Inserido
-                            mensagem: "Produtor Cadastrado",
-                            idProdutor: result
-                        })
-                    }
-                )
-            })     
+                }
+                })
+
+        })     
 })
 
 
--- Codding/routes/propriedade.js
+++ Codding/routes/propriedade.js
@@ -1,7 +1,7 @@
 const express = require('express')//Estabelece dependência com o express e seu modulador de rotas
 const router = express.Router()
 const mysql = require('../config/db').pool//Estabelece depencia com a pool de conexões do banco de dados
-
+const auth = require('basic-auth')//Estabelece dependencia com o basic auth
 
 
 
@@ -34,30 +34,41 @@ router.get('/listar',(req,res,next)=>{
 
 /*CADASTRAR NOVA PROPRIEDADE*/
 router.post('/cadastrar',(req,res,next)=>{ //Inserir Nova Propriedade
+    const user = auth(req)
     mysql.getConnection((err,con)=>{//Conectar com MySql
         if(err){//Retorna erro em caso de existir
             res.send({
                 error: err
             })
         }
-        con.query( 
-            'INSERT INTO propriedades (nomePropriedade, cadastroRural, idProdutor, idUsuario) VALUES (?,?,?,?)',//Insere na Base de Dados os valores que serão enviados pelo POST
-            [req.body.nomePropriedade, req.body.cadastroRural, req.body.idProdutor, req.body.idUsuario],
-            (err,result,fields)=>{
-                con.release()
-                if(err){//Tratativa de Erro
-                    return res.status(500).send({
-                        error: err,
-                        response: result
-                        })
+        con.query('SELECT idUsuario FROM usuarios WHERE nomeUsuario = (?)',
+                [user.name],(err,result,fields)=>{
+                    con.release()
+                    for(let i = 0;i<result.length;i++){
+                    if(result[0].idUsuario!==undefined){
+                            const idUsuario = result[0].idUsuario
+                            con.query( 
+                                'INSERT INTO propriedades (nomePropriedade, cadastroRural, idProdutor, idUsuario) VALUES (?,?,?,?)',//Insere na Base de Dados os valores que serão enviados pelo POST
+                                [req.body.nomePropriedade, req.body.cadastroRural, req.body.idProdutor, idUsuario],
+                                (err,result,fields)=>{
+                                    con.release()
+                                    if(err){//Tratativa de Erro
+                                        return res.status(500).send({
+                                            error: err,
+                                            response: result
+                                            })
+                                    }
+                                    res.status(201).send({//Retorna mensagem positiva e o id Inserido
+                                        mensagem: "Propriedade Cadastrado",
+                                        idPropriedade: result.insertId
+                                    })
+                                }
+                            )
+                        }
+                    }
                 }
-                res.status(201).send({//Retorna mensagem positiva e o id Inserido
-                    mensagem: "Propriedade Cadastrado",
-                    idPropriedade: result.insertId
-                })
-            }
-        )}
-    )     
+        )
+    })     
 })
 
 
